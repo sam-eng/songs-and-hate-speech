e6c29aaa2c667cf5628f1ad671a41431d8fb391b
sam-eng <sme388@nyu.edu>
2019-12-09 14:48:41 -0500

    add more songs, through 2005-08-13

--- genius.py
+++ genius.py
@@ -8,6 +8,7 @@ import billboard
 import time
 import sys
 from requests.exceptions import HTTPError
+from requests.exceptions import ConnectionError
 import lyricsgenius
 import nltk
 import re
@@ -28,8 +29,8 @@ Return: list of tweets that contain a 4+ word phrase identified as characteristi
 '''
 def find_songs():
     r_songs = {}
-    #start at 2007-03-10
-    r_chart = billboard.ChartData('hot-rap-tracks', '2007-03-10')
+    #start at 2005-08-13
+    r_chart = billboard.ChartData('hot-rap-tracks', '2005-08-13')
     while r_chart.previousDate:
         for song in r_chart:
             r_songs[song.title] = song.artist
