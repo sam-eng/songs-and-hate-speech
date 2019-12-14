6662201107b039e749e46223d73100cfc605fdc9
sam-eng <sme388@nyu.edu>
2019-12-14 16:03:41 -0500

    add lyrics files

--- genius.py
+++ genius.py
@@ -17,6 +17,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.collocations import *
 from nltk.probability import FreqDist
+import json
 '''
 Current issues: having trouble creating data set due to rate limits on server. Not quite sure what the limits are, so it's challenging.
 
@@ -27,10 +28,11 @@ TODO: Store lyrics (and n-grams) in text files
 Return: list of tweets that contain a 4+ word phrase identified as characteristic of song lyrics
 
 '''
+
+# Using Billboard.py, get information of at least 150 songs in the Billboard hot rap tracks chart
 def find_songs():
     r_songs = {}
-    #start at 2000-04-22
-    r_chart = billboard.ChartData('hot-rap-tracks', '2000-04-22')
+    r_chart = billboard.ChartData('hot-rap-tracks')
     while r_chart.previousDate:
         for song in r_chart:
             r_songs[song.title] = song.artist
@@ -38,7 +40,7 @@ def find_songs():
             r_chart = billboard.ChartData('hot-rap-tracks', r_chart.previousDate)
             time.sleep(2)
             print(r_chart.previousDate)
-            # have a date limit
+            # If date limit is reached, there are no more dates, or 150 song titles have been collected, stop.
             if (r_chart.previousDate is not None and r_chart.previousDate < "1990-01-01"):
                 print("DONE")
                 with open('song-titles.txt', 'a+') as f:
@@ -73,29 +75,45 @@ def find_songs():
             print("CONNECTION ERROR")
             print("final (prev) date: " + r_chart.previousDate)
             break
-'''
-def find_lyrics():
 
+# Using LyricsGenius, fetch stored information about songs, search for their lyrics in 
+def find_lyrics():
+    data = {}
     # Get songs from song-titles.txt --> split by newline, then split by tab
     # keep dictionary of song/artist to track and ignore duplicates
     with open("song-titles.txt") as f:
-        data = f.read()
-        songs = data.split("\n")
-
-    r_songs["Loyal"] = "Chris Brown"
+        s_data = f.read()
+        songs = s_data.split("\n")
+        print(songs)
+    for song in songs:
+        info = song.split("\t")
+        if info[0] == "Sunflower (Spider-Man: Into The Spider-Verse)":
+            info[0] = "Sunflower"
+        data[info[0]] = {}
+        data[info[0]]["author"] = info[1]
+    #sys.exit()
     genius = lyricsgenius.Genius("xNYwq4ZDn6Ytqu7pJ7Jvy-84TIuWLgwQTDQZ7cMe1ykdDIY7OwSCnblRToxw5N1y")
-    r_lyrics = {}
-    for song in r_songs:
-        print(r_songs[song])
-        g_song = genius.search_song(song, r_songs[song])
-        r_lyrics[song] = g_song.lyrics
+    lyrics = {}
+    for song in data:
+        try: 
+            g_song = genius.search_song(song, data[song]["author"])
+            if g_song is None:
+                print("yikes! there's an issue with this one")
+                with open("song-title-issues.txt", "a+") as f:
+                    f.write(song + "\n")
+                continue
+            lyrics[song] = g_song.lyrics
+        except: 
+            print("yikes! there's an issue with this one")
+            with open("song-title-issues.txt", "a+") as f:
+                f.write(song + "\n")
+            continue
 
-    l_stop_words = ["whoa", "woah", "yeah", "yea", "ah", "ha"]
+    lyric_stop_words = ["whoa", "woah", "yeah", "yea", "ah", "ha"]
 
-    # process lyrics
-    for song in r_lyrics:
-        
-        split_lyrics = r_lyrics[song].split("\n")
+    # Process lyrics
+    for song in lyrics:
+        split_lyrics = lyrics[song].split("\n")
         new_lyrics = []
         # Remove lines entirely in brackets and parenthesis
         for i in range(len(split_lyrics)):
@@ -108,6 +126,15 @@ def find_lyrics():
                 continue
             else:
                 new_lyrics.append(line)
+        #print(new_lyrics)
+        # Save these lightly processed lyrics in the dictionary
+        data[song]["lyrics"] = new_lyrics
+        print(song)
+    with open("song-info.txt", "w+") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    for song in data:
+        '''
+        song_lyrics = 
         parsed_lyrics = []
         for i in range(len(new_lyrics)):
             line = new_lyrics[i]
@@ -115,7 +142,7 @@ def find_lyrics():
             line = re.sub(r'\([^()]*\)', '', line)
             # REMOVE PUNCTUATION
             line = line.translate(str.maketrans('', '', string.punctuation)) 
-            line = re.sub(r'([Yy]eah(ah)?)|([Ww]hoa)|([Oo]+h)', '', line).strip() #won't remove ooh, remove Ha's
+            line = re.sub(r'([Yy]eah(ah)?)|([Ww]hoah?|(([Oo])+h)', '', line).strip() #won't remove ooh, remove Ha's
             print(line)
             if len(line) != 0:
                 parsed_lyrics.append(line)
@@ -150,8 +177,9 @@ def find_lyrics():
                 print(add)
                 break
             else:
-                continue'''
-
+                continue
+        '''    
+'''
 # calculate the most characteristic lines of the song
 # pick top N (n = 2 or 3?) lines and save those
 # compare tweets to those top lines
@@ -160,8 +188,10 @@ def find_lyrics():
 # strip blank lines
 # strip []
 # strip ()
-
+'''
 if __name__ == "__main__":
-    find_songs()
+    #with open("song-titles.txt", 'a+') as f:
+    #    f.write("Gimme Some More\tBusta Rhymes")
+    find_lyrics()
     #r_chart = billboard.ChartData('hot-rap-tracks', '1999-02-20')
     #print()
